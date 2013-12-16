8ded5d95c2a30708f7dcfe23f180af5e5776669e
Yuji Tanimoto <s11t250@stmail.eng.kagawa-u.ac.jp>
2013-12-16 13:23:12 +0900

    tabbar

---
+++ .emacs.d/inits/windows-40-tabbar.el
@@ -0,0 +1,62 @@
+(require 'tabbar)
+(tabbar-mode)
+(global-set-key "\M-]" 'tabbar-forward)  ; 次のタブ
+(global-set-key "\M-[" 'tabbar-backward) ; 前のタブ
+;; タブ上でマウスホイールを使わない
+(tabbar-mwheel-mode nil)
+;; グループを使わない
+(setq tabbar-buffer-groups-function nil)
+;; 左側のボタンを消す
+(dolist (btn '(tabbar-buffer-home-button
+               tabbar-scroll-left-button
+               tabbar-scroll-right-button))
+  (set btn (cons (cons "" nil)
+                 (cons "" nil))))
+;; 色設定
+(setq color1 "white")
+(setq color2 "dim gray")
+(setq color3 "black")
+(set-face-attribute ; バー自体の色
+  'tabbar-default nil
+   :background color2
+   :foreground color2
+   :height 0.8)
+(set-face-attribute ; アクティブなタブ
+  'tabbar-selected nil
+   :background color3
+   :foreground color1
+   :box nil
+   )
+(set-face-attribute ; 非アクティブなタブ
+  'tabbar-unselected nil
+   :background color2
+   :foreground color1
+   :box nil
+   )
+
+;; タブに表示させる内容
+(defvar my-tabbar-displayed-buffers
+  '("*scratch*" "*Messages*" "*Backtrace*" "*Colors*" "*Faces*" "*vc-")
+  "*Regexps matches buffer names always included tabs.")
+
+(defun my-tabbar-buffer-list ()
+  "Return the list of buffers to show in tabs.
+Exclude buffers whose name starts with a space or an asterisk.
+The current buffer and buffers matches `my-tabbar-displayed-buffers'
+are always included."
+  (let* ((hides (list ?\  ?\*))
+         (re (regexp-opt my-tabbar-displayed-buffers))
+         (cur-buf (current-buffer))
+         (tabs (delq nil
+                     (mapcar (lambda (buf)
+                               (let ((name (buffer-name buf)))
+                                 (when (or (string-match re name)
+                                           (not (memq (aref name 0) hides)))
+                                   buf)))
+                             (buffer-list)))))
+    ;; Always include the current buffer.
+    (if (memq cur-buf tabs)
+        tabs
+      (cons cur-buf tabs))))
+
+(setq tabbar-buffer-list-function 'my-tabbar-buffer-list)
