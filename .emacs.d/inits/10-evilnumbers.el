e180f239c80e1057f1a1a44f71d4eb36d68e4b53
tanimoto <s11t250@stmail.eng.kagawa-u.ac.jp>
2014-03-09 17:57:38 +0900

    evil-numbers導入

---
+++ .emacs.d/inits/10-evilnumbers.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path (expand-file-name "~/emacs/.emacs.d/elisp/"))
+(require 'evil-numbers)
+;; (smartrep-define-key
+;;     global-map "C-c" '(("+" . 'evil-numbers/inc-at-pt)
+;;                        ("-" . 'evil-numbers/dec-at-pt)))
+(global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
+(global-set-key (kbd "C-c -") 'evil-numbers/dec-at-pt)
