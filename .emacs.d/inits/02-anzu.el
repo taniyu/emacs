3b54c462554265826e838fe1928a3b68a8c1c7ef
taniyu <taniyugit@gmail.com>
2014-04-14 00:14:34 +0900

    anzuの導入

---
+++ .emacs.d/inits/02-anzu.el
@@ -0,0 +1,14 @@
+(add-to-list 'load-path (expand-file-name "~/emacs/.emacs.d/elisp/"))
+
+(require 'anzu)
+
+(global-anzu-mode +1)
+(set-face-attribute 'anzu-mode-line nil
+                    :foreground "yellow" :weight 'bold)
+
+(custom-set-variables
+ '(anzu-mode-lighter "")
+ '(anzu-deactivate-region t)
+ '(anzu-search-threshold 1000))
+;;(global-set-key (kbd "C-c r") 'anzu-query-replace)
+;;(global-set-key (kbd "C-c R") 'anzu-query-replace-regexp)
