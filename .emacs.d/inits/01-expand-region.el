8fec9c97d3b1b87109d82ae16e2ecde1d379019d
taniyu <taniyugit@gmail.com>
2014-04-14 00:15:35 +0900

    expand-regionの導入

---
+++ .emacs.d/inits/01-expand-region.el
@@ -0,0 +1,7 @@
+﻿;; expand region
+(add-to-list 'load-path "~/emacs/.emacs.d/plugins/expand-region.el")
+(require 'expand-region)
+(global-set-key (kbd "C-@") 'er/expand-region)
+(global-set-key (kbd "C-M-@") 'er/contract-region) ;; リージョンを狭める
+
+(transient-mark-mode t)
