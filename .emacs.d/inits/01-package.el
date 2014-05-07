4e83e36041c3830e726b740a4545fdf9d6eca413
tanimoto <s11t250@stmail.eng.kagawa-u.ac.jp>
2014-05-07 20:23:48 +0900

    package.elの導入 #4

---
+++ .emacs.d/inits/01-package.el
@@ -0,0 +1,30 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+
+;; 自動インストールの設定
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    ;; 使いたいパッケージ名を記述
+    auto-complete
+    yasnippet
+    helm
+    helm-descbinds
+    yaml-mode
+    slim-mode
+    js2-mode
+    magit
+    gitconfig-mode
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
--- .emacs.d/inits/30-yasnippet.el
+++ .emacs.d/inits/30-yasnippet.el
@@ -1,22 +1,29 @@
-(add-to-list 'load-path "~/emacs/.emacs.d/plugins/yasnippet-0.6.1c")
+;; (add-to-list 'load-path "~/emacs/.emacs.d/plugins/yasnippet-0.6.1c")
 (require 'yasnippet)
 ;;(require 'anything-c-yasnippet)
-(yas/global-mode 1)
-(yas/initialize)
-(yas/load-directory "~/emacs/.emacs.d/plugins/yasnippet-0.6.1c/snippets/")
-(yas/load-directory "~/emacs/.emacs.d/plugins/snippet")
-(yas/load-directory "~/emacs/.emacs.d/plugins/yasnippets_rails/rails-snippets")
-
+;; (yas-global-mode 1)
+;; (yas-initialize)
+;; (yas/load-directory "~/emacs/.emacs.d/plugins/yasnippet-0.6.1c/snippets/")
+;; (yas/load-directory "~/emacs/.emacs.d/plugins/snippet")
+;; (yas/load-directory "~/emacs/.emacs.d/plugins/yasnippets_rails/rails-snippets")
 ;;(setq yas/prompt-functions '(yas/dropdown-prompt))
 
-(setq yas/next-field-key "TAB")
-(setq yas/prev-field-key "<s-tab>")
+(setq yas-snippet-dirs
+      '("~/emacs/.emacs.d/plugins/snippet"
+        "~/emacs/.emacs.d/plugins/yasnippets_rails/rails-snippets"
+        "~/.emacs.d/elpa/yasnippet-20140427.1224/snippets"
+        ))
+(yas-global-mode 1)
+;; (yas-initialize)
+
+;; (setq yas/next-field-key "TAB")
+;; (setq yas/prev-field-key "<s-tab>")
 ;; 既存スニペットを挿入
-(define-key yas/minor-mode-map (kbd "C-x y i") 'yas/insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-x y i") 'yas-insert-snippet)
 ;; 新規スニペットを作成するバッファを用意する
-(define-key yas/minor-mode-map (kbd "C-x y n") 'yas/new-snippet)
+(define-key yas-minor-mode-map (kbd "C-x y n") 'yas-new-snippet)
 ;; 既存スニペットの閲覧、編集
-(define-key yas/minor-mode-map (kbd "C-x y v") 'yas/visit-snippet-file)
+(define-key yas-minor-mode-map (kbd "C-x y v") 'yas-visit-snippet-file)
 
 ;; anything interface
 (eval-after-load "helm-config"
