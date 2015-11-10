03b63673848755ad2d32f35dc67ac6ecb57c764a
Yuji Tanimoto <y.tanimoto13@gmail.com>
2015-11-10 13:40:26 +0900

    マジックコメントを入れないようにする

--- .emacs.d/inits/30-rubymode.el
+++ .emacs.d/inits/30-rubymode.el
@@ -18,3 +18,5 @@
 (require 'ruby-electric)
 (add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
 (setq ruby-electric-expand-delimiters-list nil)
+;; encodeを入力しないようにする
+(defun ruby-mode-set-encoding () nil)
