4257ac45ca43e2a794611a19e6ea5de22b6b1be7
Yuji Tanimoto <y.tanimoto13@gmail.com>
2016-06-20 14:53:43 +0900

    キーバインド追加

--- .emacs.d/inits/10-key-chord.el
+++ .emacs.d/inits/10-key-chord.el
@@ -1,6 +1,9 @@
 (require 'key-chord)
 (key-chord-mode 1)
-(setq key-chord-two-keys-delay 0.03)
+(setq key-chord-two-keys-delay 0.04)
 
 (key-chord-define-global "gl" 'goto-line)
 (key-chord-define-global "gr" 'helm-git-grep)
+(key-chord-define-global "rp" 'query-replace-regexp)
+(key-chord-define-global "fk" 'helm-for-files)
+(key-chord-define-global "fj" 'helm-M-x)
--- .emacs.d/inits/30-guidekey.el
+++ .emacs.d/inits/30-guidekey.el
@@ -1,3 +1,4 @@
 (require 'guide-key)
 (setq guide-key/guide-key-sequence '("C-c p"))
+(setq guide-key/guide-key-sequence '("C-c r"))
 (guide-key-mode 1)  ; guide-key-mode を有効にする
