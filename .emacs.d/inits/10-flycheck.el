600cf8d24a329a78903f038db3e87965fd361ea7
Yuji Tanimoto <y.tanimoto13@gmail.com>
2016-08-10 13:50:24 +0000

    eslint対応

--- .emacs.d/inits/10-flycheck.el
+++ .emacs.d/inits/10-flycheck.el
@@ -1,2 +1,14 @@
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;;(add-hook 'after-init-hook #'global-flycheck-mode)
 
+(eval-after-load 'flycheck
+  '(custom-set-variables
+    '(flycheck-disabled-checkers '(javascript-jshint javascript-jscs))
+    ))
+
+
+;; flycheck jsx eslint
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (equal web-mode-content-type "jsx")
+              (flycheck-add-mode 'javascript-eslint 'web-mode)
+              (flycheck-mode))))
