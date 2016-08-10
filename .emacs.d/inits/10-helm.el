13e4596b6eb28dea10a21ec294f12ad5c05e6d21
Yuji Tanimoto <y.tanimoto13@gmail.com>
2016-08-10 23:14:45 +0900

    helmのキーバインド追加

--- .emacs.d/inits/10-helm.el
+++ .emacs.d/inits/10-helm.el
@@ -6,7 +6,7 @@
 (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
 (define-key global-map (kbd "C-c i")   'helm-imenu)
 (define-key global-map (kbd "C-x b")   'helm-buffers-list)
-(define-key global-map (kbd "C-c h g")   'helm-git-grep)
+(define-key global-map (kbd "C-c g g")   'helm-git-grep)
 (define-key global-map (kbd "C-c h f")   'helm-for-files)
 
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
--- .emacs.d/inits/10-key-chord.el
+++ .emacs.d/inits/10-key-chord.el
@@ -2,11 +2,6 @@
 (key-chord-mode 1)
 (setq key-chord-two-keys-delay 0.04)
 
-(key-chord-define-global "gl" 'goto-line)
-(key-chord-define-global "gr" 'helm-git-grep)
-(key-chord-define-global "qr" 'query-replace-regexp)
-(key-chord-define-global "fk" 'helm-for-files)
-(key-chord-define-global "fj" 'helm-M-x)
 (key-chord-define-global "uu" (lambda ()
                                 (interactive)
                                 (switch-to-buffer nil)))
