2da5706ffe25710d624b6bb94c0d9a3e3933e8c0
Yuji Tanimoto <y.tanimoto13@gmail.com>
2016-11-17 23:14:53 +0900

    add new keybind

--- .emacs.d/inits/02-expand-region.el
+++ .emacs.d/inits/02-expand-region.el
@@ -2,6 +2,7 @@
 (use-package expand-region
   :bind (("C-c e r" . er/expand-region)
          ("C-c c r" . er/contract-region)
+         ("C-t" . er/expand-region)
          )
   :config
   (transient-mark-mode t)
